140cc842ae908b56a19b365bed8a770c5087875c
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-22 00:42:44 -0400

    set all formatters that are likely to get used

--- treefmt.nix
+++ treefmt.nix
@@ -5,11 +5,26 @@
 }:
 {
   projectRootFile = "flake.nix";
-  programs.nixfmt.enable = true;
-  programs.jsonfmt.enable = true;
-  programs.shellcheck.enable = true;
 
-  # buggy as of right now
-  # programs.nufmt.enable = true;
-  # settings.formatter.nufmt.includes = lib.mkAfter [ "pre-commit" ];
+  programs = {
+    nixfmt.enable = true;
+    jsonfmt.enable = true;
+    shellcheck.enable = true;
+    gofmt.enable = true;
+    ruff.enable = true;
+    yamlfmt.enable = true;
+    toml-sort.enable = true;
+    dos2unix.enable = true;
+    keep-sorted.enable = true;
+    # buggy as of right now
+    # nufmt.enable = true;
+  };
+
+  settings = {
+    # files to exlude from all formatting
+    excludes = [ ];
+    formatter = {
+      # formatter-specific settings
+    };
+  };
 }
